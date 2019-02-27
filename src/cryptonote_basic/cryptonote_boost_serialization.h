8f8b3f12483132c5c4a5baf86b7509229560347b
Marko Atanasievski <markoatana@gmail.com>
2019-02-27 13:42:27 +0100

    Update transaction serialization

--- src/cryptonote_basic/cryptonote_boost_serialization.h
+++ src/cryptonote_basic/cryptonote_boost_serialization.h
@@ -192,16 +192,7 @@ namespace boost
     a & x.vin;
     a & x.vout;
     a & x.extra;
-    if (x.version == 1)
-    {
-      a & x.signatures;
-    }
-    else
-    {
-      a & (rct::rctSigBase&)x.rct_signatures;
-      if (x.rct_signatures.type != rct::RCTTypeNull)
-        a & x.rct_signatures.p;
-    }
+    a & x.signatures;
   }
 
   template <class Archive>
