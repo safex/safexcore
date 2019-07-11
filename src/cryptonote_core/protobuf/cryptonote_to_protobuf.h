faa8705bf7624266405bb2a6dc271d1a8077ad3a
Stefan Isidorovic <teva.biz@gmail.com>
2019-07-11 12:57:41 +0200

    Protobuf block hash changes

--- src/cryptonote_core/protobuf/cryptonote_to_protobuf.h
+++ src/cryptonote_core/protobuf/cryptonote_to_protobuf.h
@@ -98,7 +98,7 @@ namespace safex {
         // @brief Getting all necessary data for block and serializing.
         // @param reference to cryptonote::block
         // @param list of transactions in given block
-        void add_block(const cryptonote::block& blck);
+        void add_block(const cryptonote::block& blck, crypto::hash& hash);
         void add_error(const std::string& err);
 
         // @brief Get string representation of protobuf serialization.
@@ -109,7 +109,7 @@ namespace safex {
 
         // @brief Getting protobuf BlockHeader structur from cryptonote::block
         // @0param reference to cryptnote::block
-        safex::BlockHeader* proto_block_header(const cryptonote::block& blck);
+        safex::BlockHeader* proto_block_header(const cryptonote::block& blck,  crypto::hash& hash);
 
     };
 
