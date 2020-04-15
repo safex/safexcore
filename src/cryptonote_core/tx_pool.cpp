3e9a0f70aa5b62cca5cc235fba2c945e7ec3cff7
Igor Grkavac <grkavac.igor@gmail.com>
2020-04-16 01:25:08 +0200

    Add kept_by_block for safex checks also

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -195,7 +195,7 @@ namespace cryptonote
       }
 
       //Here check for tx related safex logic
-      if ((tx.version > HF_VERSION_MIN_SUPPORTED_TX_VERSION) && !m_blockchain.check_safex_tx(tx, tvc))
+      if ( !kept_by_block && (tx.version > HF_VERSION_MIN_SUPPORTED_TX_VERSION) && !m_blockchain.check_safex_tx(tx, tvc))
       {
         tvc.m_verifivation_failed = true;
         tvc.m_safex_verification_failed = true;
