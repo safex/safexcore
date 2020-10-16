fbee1c303194dd46d2337c4ba983171c13a2f730
Igor Grkavac <grkavac.igor@gmail.com>
2020-10-16 14:00:45 +0200

    [core_tests] Added tests for token double spend

--- tests/core_tests/chaingen_main.cpp
+++ tests/core_tests/chaingen_main.cpp
@@ -167,6 +167,20 @@ int main(int argc, char* argv[])
     GENERATE_AND_PLAY(gen_double_spend_in_alt_chain_in_different_blocks<false>);
     GENERATE_AND_PLAY(gen_double_spend_in_alt_chain_in_different_blocks<true>);
 
+    // Token double spend
+    GENERATE_AND_PLAY(gen_double_spend_token_in_tx<false>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_tx<true>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_the_same_block<false>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_the_same_block<true>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_different_blocks<false>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_different_blocks<true>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_different_chains);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_alt_chain_in_the_same_block<false>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_alt_chain_in_the_same_block<true>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_alt_chain_in_different_blocks<false>);
+    GENERATE_AND_PLAY(gen_double_spend_token_in_alt_chain_in_different_blocks<true>);
+
+
     GENERATE_AND_PLAY(gen_uint_cash_overflow_1);
     GENERATE_AND_PLAY(gen_uint_cash_overflow_2);
     GENERATE_AND_PLAY(gen_uint_token_overflow_1);
@@ -203,7 +217,6 @@ int main(int argc, char* argv[])
 
 #else
 
-    GENERATE_AND_PLAY(gen_simple_chain_split_safex);
 
 
 #endif
--- tests/core_tests/chaingen_tests_list.h
+++ tests/core_tests/chaingen_tests_list.h
@@ -37,6 +37,7 @@
 #include "chain_split_1.h"
 #include "chain_switch_1.h"
 #include "double_spend.h"
+#include "double_spend_token.h"
 #include "integer_overflow.h"
 #include "ring_signature_1.h"
 #include "tx_validation.h"
